f4e4ab171e84becef0e5c9917b2311a3e5d2f0a5
Lei Xu <leonard.xu.thu@gmail.com>
2016-10-16 22:36:24 +0800

    ICNN-RL: update scripts.

--- RL/script/Hopper.sh
+++ RL/script/Hopper.sh
@@ -4,30 +4,31 @@ trainTimestep=1000
 testEpisode=1
 task="Hopper-v1"
 outdir="output/Hopper-v1/"
+monitor=-1
 
 #DDPG
 for ((i=0;i<$N;i++))
 do
   python src/main.py --model DDPG --env $task --outdir $outdir/DDPG/$i \
-    --total $total --train $trainTimestep --test $testEpisode --reward_k 0.2\
-    --tfseed $i --gymseed $i --npseed $i
+    --total $total --train $trainTimestep --test $testEpisode \
+    --tfseed $i --gymseed $i --npseed $i --monitor $monitor --l2norm 0.001
 done
 
 #NAF
 for ((i=0;i<$N;i++))
 do
   python src/main.py --model NAF --env $task --outdir $outdir/NAF/$i \
-    --total $total --train $trainTimestep --test $testEpisode --reward_k 0.05\
-    --tfseed $i --gymseed $i --npseed $i
+    --total $total --train $trainTimestep --test $testEpisode --reward_k 0.3\
+    --tfseed $i --gymseed $i --npseed $i  --monitor $monitor  --l2norm 0.001
 done
 
 #ICNN
 for ((i=0;i<$N;i++))
 do
   python src/main.py --model ICNN --env $task --outdir $outdir/ICNN/$i \
-    --total $total --train $trainTimestep --test $testEpisode --reward_k 0.2\
-    --tfseed $i --gymseed $i --npseed $i
+    --total $total --train $trainTimestep --test $testEpisode --reward_k 0.1\
+    --tfseed $i --gymseed $i --npseed $i --monitor $monitor --l2norm 0.001 --icnn_bn True
 done
 
 #plot
-python src/plot.py --runs $N --total $total --train $trainTimestep --data $outdir
+python src/plot.py --runs $N --total $total --train $trainTimestep --data $outdir --min -500 --max 2500
--- RL/script/InvertedPendulum.sh
+++ RL/script/InvertedPendulum.sh
@@ -1,16 +1,17 @@
-N=2
+N=10
 total=10000
 trainTimestep=100
 testEpisode=1
 task="InvertedPendulum-v1"
 outdir="output/InvertedPendulum-v1/"
+monitor=-1
 
 #DDPG
 for ((i=0;i<$N;i++))
 do
   python src/main.py --model DDPG --env $task --outdir $outdir/DDPG/$i \
     --total $total --train $trainTimestep --test $testEpisode --reward_k 0.3 \
-    --tfseed $i --gymseed $i --npseed $i
+    --tfseed $i --gymseed $i --npseed $i --monitor $monitor
 done
 
 #NAF
@@ -18,7 +19,7 @@ for ((i=0;i<$N;i++))
 do
   python src/main.py --model NAF --env $task --outdir $outdir/NAF/$i \
     --total $total --train $trainTimestep --test $testEpisode --reward_k 0.2 \
-    --tfseed $i --gymseed $i --npseed $i &
+    --tfseed $i --gymseed $i --npseed $i --monitor $monitor
 done
 
 #ICNN
@@ -26,8 +27,8 @@ for ((i=0;i<$N;i++))
 do
   python src/main.py --model ICNN --env $task --outdir $outdir/ICNN/$i \
     --total $total --train $trainTimestep --test $testEpisode --reward_k 0.3\
-    --tfseed $i --gymseed $i --npseed $i &
+    --tfseed $i --gymseed $i --npseed $i --monitor $monitor
 done
 
 #plot
-python src/plot.py --runs $N --total $total --train $trainTimestep --data $outdir
+python src/plot.py --runs $N --total $total --train $trainTimestep --data $outdir --min 0 --max 1100
