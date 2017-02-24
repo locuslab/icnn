f07242a3eaeba63e4f51b4c77e398fc49297ef05
Brandon Amos <bamos@cs.cmu.edu>
2017-02-23 22:30:13 -0500

    Update analysis to report multiple stats.

--- RL/analyze.random-search.py
+++ RL/analyze.random-search.py
@@ -11,6 +11,8 @@ import operator
 import numpy as np
 import numpy.random as npr
 
+import pandas as pd
+
 pythonCmd = 'python3'
 rlDir = os.path.dirname(os.path.realpath(__file__))
 plotSrc = os.path.join(rlDir, 'plot-all.py')
@@ -23,35 +25,93 @@ def main():
     parser.add_argument('--expDir', type=str, default='output.random-search')
     args = parser.parse_args()
 
-    bestParams, bestVals = {}, {}
+    task_dfs = {}
     for task in os.listdir(args.expDir):
         taskDir = os.path.join(args.expDir, task)
         if os.path.isdir(taskDir):
-            bestParams[task], bestVals[task] = analyzeTask(taskDir)
+            alg_dfs = analyzeTask(taskDir)
+            task_dfs[task] = alg_dfs
 
-    orgTableP = os.path.join(args.expDir, 'table.org')
+    makeTable(args, 'cumulativeTestRew', task_dfs)
+    makeTable(args, 'maxTestRew', task_dfs)
+    makeTable(args, 'finalTestRew', task_dfs)
+
+    # bestParamsP = os.path.join(args.expDir, 'bestParams.json')
+    # with open(bestParamsP, 'w') as f:
+    #     json.dump(bestParams, f, indent=2, sort_keys=True)
+    # print('Created {}'.format(bestParamsP))
+
+def makeTable(args, tag, task_dfs):
+    orgTableP = os.path.join(args.expDir, '{}.table.org'.format(tag))
     with open(orgTableP, 'w') as f:
         f.write('| Task | DDPG | NAF | ICNN |\n')
         f.write('|------+------+-----+------|\n')
-        for task, algs in sorted(bestVals.items()):
-            bestAlg = sorted(algs.items(), key=operator.itemgetter(1),
-                             reverse=True)[0][0]
+        for task, alg_dfs in sorted(task_dfs.items()):
+            stats  = []
+            for alg in all_algs:
+                exps = alg_dfs[alg]
+                stats.append(exps[tag].max())
+            bestStat = max(stats)
 
-            def getStr(alg):
-                s = '{:.2f} ({:.2f})'.format(algs[alg][0], int(algs[alg][1]))
-                if alg == bestAlg:
+            def getStr(val):
+                s = '{:.2f}'.format(val)
+                if (bestStat - val)/bestStat <= 0.01:
                     s = '*{}*'.format(s)
                 return s
 
             f.write('| {:s} | {} | {} | {} |\n'.format(
-                task, getStr('DDPG'), getStr('NAF'), getStr('ICNN')))
+                *([task]+list(map(getStr, stats)))))
             f.flush()
     print('Created {}'.format(orgTableP))
 
-
-    texTableP = os.path.join(args.expDir, 'table.tex')
+    texTableP = os.path.join(args.expDir, '{}.table.tex'.format(tag))
     os.system('pandoc {} --to latex --output {}'.format(orgTableP, texTableP))
 
+
+def analyzeTask(taskDir):
+    alg_dfs = {}
+    for alg in all_algs:
+        algDir = os.path.join(taskDir, alg)
+        if os.path.exists(algDir):
+            exps = []
+            for exp in sorted(os.listdir(algDir)):
+                expDir = os.path.join(algDir, exp)
+                testData = np.loadtxt(os.path.join(expDir, 'test.log'))
+                testRew = testData[:,1]
+
+                N = 10
+
+                if np.any(np.isnan(testRew)) or testRew.size <= N:
+                    continue
+
+                testRew = rolling(N, testRew)
+                d = {
+                    'exp': exp,
+                    'maxTestRew': testRew.max(),
+                    'finalTestRew': testRew[-1],
+                    'cumulativeTestRew': testRew[-1],
+                }
+                exps.append(d)
+
+            df = pd.DataFrame(exps)
+            df = df.set_index('exp')
+            alg_dfs[alg] = df
+
+    return alg_dfs
+
+def rolling(N, rew):
+    K = np.full(N, 1./N)
+    return np.convolve(rew, K, 'valid')
+
+
+def getParams(p):
+    # params = getParams(os.path.join(algDir, bestExp, 'flags.json'))
+    with open(flagsP, 'r') as flagsF:
+        f.write(flagsF.read()+'\n')
+        flagsF.seek(0)
+        flags = json.load(flagsF)
+
+    assert(False)
     for task, algs in bestParams.items():
         for alg, params in algs.items():
             del params['copy']
@@ -66,56 +126,6 @@ def main():
             del params['summary']
             del params['tfseed']
             del params['thread']
-
-    bestParamsP = os.path.join(args.expDir, 'bestParams.json')
-    with open(bestParamsP, 'w') as f:
-        json.dump(bestParams, f, indent=2, sort_keys=True)
-    print('Created {}'.format(bestParamsP))
-
-def analyzeTask(taskDir):
-    bestParams = {}
-    bestVals = {}
-    print('=== {}'.format(taskDir))
-    with open(os.path.join(taskDir, 'analysis.txt'), 'w') as f:
-        for alg in all_algs:
-            algDir = os.path.join(taskDir, alg)
-            if os.path.exists(algDir):
-                f.write('\n=== {}\n\n'.format(alg))
-                exps = {}
-                for exp in sorted(os.listdir(algDir)):
-                    expDir = os.path.join(algDir, exp)
-                    testData = np.loadtxt(os.path.join(expDir, 'test.log'))
-                    testRew = testData[:,1]
-
-                    N = 10
-
-                    if np.any(np.isnan(testRew)) or testRew.size <= N:
-                        continue
-
-                    testRew_ = np.array([sum(testRew[i-N:i])/N for
-                                         i in range(N, len(testRew))])
-                    exps[exp] = [testRew_[-1], testRew_.sum()]
-
-                    f.write(('  + Experiment {}: Final rolling reward of {} '+
-                             'with a cumulative reward of {}\n').format(
-                                 *([exp] + exps[exp])))
-
-                s = sorted(exps.items(), key=operator.itemgetter(1), reverse=True)
-                best = s[0]
-                bestExp = best[0]
-
-                f.write('\n--- Best of {} obtained in experiment {}\n'.format(
-                    best[1], bestExp))
-                flagsP = os.path.join(algDir, bestExp, 'flags.json')
-                with open(flagsP, 'r') as flagsF:
-                    f.write(flagsF.read()+'\n')
-                    flagsF.seek(0)
-                    flags = json.load(flagsF)
-
-                bestParams[alg] = flags
-                bestVals[alg] = best[1]
-
-    return bestParams, bestVals
 
 if __name__=='__main__':
     main()
